0599985b843cae3da29dfe2b01054ac0f6d9e93f
elcymon <elcymon@gmail.com>
2019-08-14 23:58:59 +0100

    setting up the navbar

--- js/script.js
+++ js/script.js
@@ -9,20 +9,29 @@ function navFunction() {
 
   function scrollInfo() {
       var e = document.documentElement.scrollTop;
-      console.log(e);
+      // console.log(e);
       document.getElementById('scroll-location').innerText = 'value:' + e;
   }
 function myPosition() {
     var loc = document.getElementsByClassName('sections');
-    var elem = 'elcymon';
+    var elem = document.getElementById('nav-active').innerText;
     for(var i = 0; i < loc.length; i++) {
         var elemLoc = loc[i];
-        if (elemLoc.getBoundingClientRect().top < 10) {
+        if (elemLoc.getBoundingClientRect().top /  document.documentElement.clientHeight < 0.3) {
+          
             elem = elemLoc.getElementsByTagName("h2")[0].innerText;
+            if (elem == 'Simon Obute') {
+              elem = 'elcymon';
+            }
+            // console.log(elem,elemLoc.getBoundingClientRect().top);
         }
     }
-    console.log('hi',loc);
-    document.getElementById('link-location').innerText = elem;
+    document.getElementById('nav-active').innerText = elem;
+    // var footer = document.getElementsByTagName('footer');
+    console.log('footer',document.getElementById('welcome-section').getBoundingClientRect().top / document.documentElement.clientHeight)
+    if (document.getElementById('nav-links').style.display === 'block') {
+      document.getElementById('nav-links').style.display = 'none';
+    }
 }
 // document.addEventListener(myPosition, 500);
 
